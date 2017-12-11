6bc62f95f47308b06a4614a8fb822c7fe77211c6
Nils Van Zuijlen <nilsdu29@gmail.com>
2017-12-11 10:34:22 +0100

    Update Curve.py

--- Curve.py
+++ Curve.py
@@ -1,9 +1,16 @@
 # -*- coding:UTF-8 -*-
+"""
+The temperature curve
+"""
 from .Segment import Segment
 
 
 class Curve:
-
+    """
+    The temperature curve
+    
+    Stores Segments and fetches the current wanted temperature in them
+    """
     def __init__(self, time):
         self.time = time
         self._current_segment = 0
@@ -21,14 +28,23 @@ class Curve:
             self._current_segment = i
 
     def get_temperature(self):
+        """
+        Fetches the current wanted temperature
+        """
         self._update_current()
         return self.get_segment().temperature_at(self.time.now())
 
     def get_segment(self):
+        """
+        Fetches the current segment
+        """
         self._update_current()
         return self._segments[self._current_segment]
 
     def add_segment(self, segment):
+        """
+        Adds segment to the end of the curve
+        """
         assert isinstance(segment, Segment)
         if len(self._segments) != 0:
             assert segment.begin == self._segments[-1].end
