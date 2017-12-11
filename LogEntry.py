2e8d3519a376ba2ac9cc3d26fb3ce9ff8cf029a3
Nils Van Zuijlen <nilsdu29@gmail.com>
2017-12-11 11:02:44 +0100

    Update LogEntry.py

--- LogEntry.py
+++ LogEntry.py
@@ -1,8 +1,17 @@
 # -*- coding:UTF-8 -*-
+"""
+Stores one log entry
+"""
 
 
 class LogEntry:
+    """
+    Stores one log entry
 
+    time: the time the log was issued
+    event: the type of event logged
+    value: the changes made during this event
+    """
     def __init__(self, time, event, value):
         self.time = time
         self.event = event
