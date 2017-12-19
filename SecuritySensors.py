ba39d4a7254baf35071a8639f8476e7bfc132ce0
Nils Van Zuijlen <nilsdu29@gmail.com>
2017-12-19 09:13:51 +0100

    Update SecuritySensors.py

--- SecuritySensors.py
+++ SecuritySensors.py
@@ -1,9 +1,10 @@
 # -*- coding:UTF-8 -*-
+"""State of the security sensors"""
 import threading
 
 
 class SecuritySensors(threading.Thread):
-
+    """State of the security sensors"""
     def __init__(self):
         self.door_state = True
         self.emergency_button_state = True
@@ -11,10 +12,13 @@ class SecuritySensors(threading.Thread):
         threading.Thread.__init__(self)
 
     def check_state(self):
+        """Checks the current state of all sensors"""
         pass
 
     def trigger(self):
+        """Triggers a security stop"""
         pass
 
     def reset(self):
+        """Resets the security"""
         pass
